9870722bc7a94db5142431cdc7855eb66fc53977
Argha Sen <senargha679@gmail.com>
2025-08-22 13:33:34 +0530

    Update Navbar.jsx

--- frontEnd/user-app/src/Navbar.jsx
+++ frontEnd/user-app/src/Navbar.jsx
@@ -36,7 +36,12 @@ const Navbar = () => {
     window.location.href = "/login"
   }
 
-
+const closeMenu = () => {
+    const navbar = document.getElementById("navbarSupportedContent"); 
+    if (navbar && navbar.classList.contains("show")) {
+      const bsCollapse = new window.bootstrap.Collapse(navbar, { toggle: false });
+      bsCollapse.hide();
+    }
 //   return (
 //     <div>
 //       <div className="container-fluid fixed-top px-0 wow fadeIn" data-wow-delay="0.1s">
@@ -110,84 +115,36 @@ const Navbar = () => {
 //     </div>
 //   )
    return (
-    <div>
-      {/* Top Bar */}
-      <div className="container-fluid fixed-top px-0 wow fadeIn" data-wow-delay="0.1s">
-        <div className="top-bar row gx-0 align-items-center d-none d-lg-flex">
-          <div className="col-lg-6 px-5 text-start">
-            <small><i className="fa fa-map-marker-alt me-2" />123 Street, New York, USA</small>
-            <small className="ms-4"><i className="fa fa-envelope me-2" />info@example.com</small>
-          </div>
-          <div className="col-lg-6 px-5 text-end">
-            <small>Follow us:</small>
-            <a className="text-body ms-3" href="#"><i className="fab fa-facebook-f" /></a>
-            <a className="text-body ms-3" href="#"><i className="fab fa-twitter" /></a>
-            <a className="text-body ms-3" href="#"><i className="fab fa-linkedin-in" /></a>
-            <a className="text-body ms-3" href="#"><i className="fab fa-instagram" /></a>
-          </div>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">FoodApp</Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link" to="/login" onClick={closeMenu}>Login</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/register" onClick={closeMenu}>Register</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/about" onClick={closeMenu}>About</Link>
+            </li>
+          </ul>
         </div>
-
-        {/* Navbar */}
-        <nav className="navbar navbar-expand-lg navbar-light bg-white shadow-sm py-lg-0 px-lg-5">
-          <Link to="/" className="navbar-brand ms-3 ms-lg-0">
-            <h1 className="fw-bold text-primary m-0">
-              F<span className="text-secondary">oo</span>dy
-            </h1>
-          </Link>
-
-          {/* Toggler for mobile */}
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarCollapse"
-            aria-controls="navbarCollapse"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-
-          {/* Links */}
-          <div className="collapse navbar-collapse" id="navbarCollapse">
-            <div className="navbar-nav ms-auto p-3 p-lg-0 text-center text-lg-start">
-              <Link to="/" className="nav-item nav-link active">Home</Link>
-              <Link to="/product" className="nav-item nav-link">Products</Link>
-
-              {/* If not logged in */}
-              {flag === 0 ? (
-                <Link to="/login" className="btn-sm-square bg-white rounded-circle ms-3">
-                  <small className="fa fa-user text-body" />
-                </Link>
-              ) : (
-                <>
-                  <Link to="/cart" className="btn-sm-square bg-white rounded-circle ms-3">
-                    <small className="fa fa-shopping-bag text-body" /><sup>{cartCount}</sup>
-                  </Link>
-                  <div className="nav-item dropdown">
-                    <a
-                      href="#"
-                      className="nav-link dropdown-toggle"
-                      id="userDropdown"
-                      role="button"
-                      data-bs-toggle="dropdown"
-                      aria-expanded="false"
-                    >
-                      {name}
-                    </a>
-                    <ul className="dropdown-menu" aria-labelledby="userDropdown">
-                      <li><Link to="/profile" className="dropdown-item">Profile</Link></li>
-                      <li><Link to="/order" className="dropdown-item">Order</Link></li>
-                      <li><a onClick={logout} className="dropdown-item" style={{ cursor: "pointer" }}>Log out</a></li>
-                    </ul>
-                  </div>
-                </>
-              )}
-            </div>
-          </div>
-        </nav>
       </div>
-    </div>
+    </nav>
   );
 }
 
